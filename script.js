6b4d7f157bc7100529f5c3dc5168b2baa075a791
Awilixsh <awilixsh@gmail.com>
2024-10-05 19:19:40 +0800

    Test

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function openNavbar(){
-
+	var imgBurger = document.getElementById('imgHamburger');
 }
 
 function openNavbar_drop(navDrop){
 	var navList = document.getElementById('navList').getElementsByTagName("li")[navDrop].getElementsByTagName('img')[0];
 	//var navList = document.getElementById('test');
+	console.log(navList.src.includes('/images/icon-arrow-up.svg);
 	if(navList.src.includes('/images/icon-arrow-up.svg')){
 		navList.src = "./images/icon-arrow-down.svg";
 	} else {
