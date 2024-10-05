04a3073ebd339a5d9d15f8f16b0e840f0ce81328
Awilixsh <awilixsh@gmail.com>
2024-10-05 23:15:49 +0800

    navigation bar in progress

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 function openNavbar(){
-	var imgBurger = document.getElementById('imgHamburger');
+	var imgButton = document.getElementById('imgHamburger');
+	var divOverlay = document.getElementById('divOverlay');
+	var navBar = document.getElementById('divNav');
+	
+	if(imgButton.src.includes('/images/icon-close-menu.svg')){
+		imgButton.src = './images/icon-menu.svg';
+		imgButton.style.zIndex = '1';
+		divOverlay.style.display = 'none';
+		navBar.style.display = 'none';
+	} else {
+		imgButton.src = './images/icon-close-menu.svg';
+		imgButton.style.zIndex = '3';
+		divOverlay.style.display = 'block';
+		navBar.style.display = 'block';
+	}
+	
+	
 }
 
 function openNavbar_drop(navDrop){
 	var navList = document.getElementById('navList').getElementsByTagName("li")[navDrop].getElementsByTagName('img')[0];
-	//var navList = document.getElementById('test');
-	console.log(navList.src);
 	if(navList.src.includes('/images/icon-arrow-up.svg')){
 		navList.src = "./images/icon-arrow-down.svg";
 	} else {
