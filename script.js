c279ab366e997917263bff0a0a62b2b00061cbeb
Awilixsh <awilixsh@gmail.com>
2024-10-07 20:52:44 +0800

    Updated navigation bar

--- script.js
+++ script.js
@@ -15,15 +15,19 @@ function openNavbar(){
 	}
 }
 
-function openNavbar_drop(navDrop){
+function openNavbar_drop(navDrop,navDrop_title){
 	var navList = document.getElementById('navList').getElementsByTagName("li")[navDrop];
 	var navList_img = navList.getElementsByTagName('img')[0];
 	var navList_sub = navList.getElementsByTagName('ul')[0];
+	var navList_title = document.getElementsByClassName('navDropdowntitle')[navDrop_title];
+	console.log(navList_title);
 	if(navList_img.src.includes('/images/icon-arrow-up.svg')){
 		navList_img.src = "./images/icon-arrow-down.svg";
 		navList_sub.style.display = 'none';
+		navList_title.style.color = 'hsl(0, 0%, 41%)'
 	} else {
 		navList_img.src = "./images/icon-arrow-up.svg";
 		navList_sub.style.display = 'block';
+		navList_title.style.color = 'hsl(0, 0%, 8%)';
 	}
 }
